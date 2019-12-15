1aae767c517080e8fa4f4800b64b061a2da9abc4
Roger Smith <nz.rogersmith@gmail.com>
2019-12-15 22:28:49 +1300

    Changed the game scene to load asynchronously due to slight loading delay on less powerful android devices

--- 2D Side Scroller/Assets/Scripts/InstructionsScreen.cs
+++ 2D Side Scroller/Assets/Scripts/InstructionsScreen.cs
@@ -5,16 +5,48 @@ using UnityEngine.SceneManagement;
 
 public class InstructionsScreen : MonoBehaviour
 {
+    public GameObject InstructionsContent;
+    public GameObject LoadingLabel;
+
+    private bool isLoadingScene = false; // Used to stop the player from attempting to load the scene more than once
+
+    /// <summary>
+    /// Processes gameplay logic immediately after objects are initialized
+    /// </summary>
+    void Awake()
+    {
+        // Set the instructions section of the menu to be shown
+        InstructionsContent.SetActive(true);
+        LoadingLabel.SetActive(false);
+    }
+
     /// <summary>
     /// Updates gameplay logic any time a new frame is displayed to the screen
     /// </summary>
     void Update()
     {
         // If the player taps the screen
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && !isLoadingScene)
         {
-            // Launch into the game
-            SceneManager.LoadScene("Game");
+            // Prevent the loading attempt from being fired a second time
+            isLoadingScene = true;
+
+            // Switch to the loading text
+            InstructionsContent.SetActive(false);
+            LoadingLabel.SetActive(true);
+
+            // Start loading the game
+            StartCoroutine(LoadGameScene());
+        }
+    }
+
+    IEnumerator LoadGameScene()
+    {
+        AsyncOperation async = SceneManager.LoadSceneAsync(1);
+
+        while (!async.isDone)
+        {
+            yield return null;
         }
     }
 }
